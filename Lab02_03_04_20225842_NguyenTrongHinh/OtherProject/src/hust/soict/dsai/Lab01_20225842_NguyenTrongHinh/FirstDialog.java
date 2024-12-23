484fdf1d4cfcf0817fdcb9f08590987e3aa65dde
NguyenTrongHinh <127172728+NguyenTrongHinh@users.noreply.github.com>
2024-12-23 19:46:34 +0700

    Delete Lab02_03_04_20225842_NguyenTrongHinh directory

--- Lab02_03_04_20225842_NguyenTrongHinh/OtherProject/src/hust/soict/dsai/Lab01_20225842_NguyenTrongHinh/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class FirstDialog{
-
-    public static void main(String[] args) {
-        JOptionPane.showConfirmDialog(null, "Nguyen Trong Hinh - 20225842 -Hello world!How are you?");
-        System.exit(0);
-    }
-}
